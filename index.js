1fa9bfb5e089dc5cfb10341650523342e492c9e0
TarasKovalyshyn <taras.kovalyshyn@gmail.com>
2023-03-02 16:41:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,3 @@
-// const contacts = require("./contacts");
-// const yargs = require("yargs");
-// const { hideBin } = require("yargs/helpers");
-
-// const invokeAction = async ({ action, id, name, email, phone }) => {
-//   switch (action) {
-//     case "list":
-//       const allContscts = await contacts.listContacts();
-//       console.log(allContscts);
-//       break;
-
-//     case "get":
-//       const oneContact = await contacts.getContactById(id);
-//       console.log(oneContact);
-//       break;
-
-//     case "add":
-//       const newContact = await contacts.addContact({ name, email, phone });
-//       console.log(newContact);
-//       break;
-
-//     case "remove":
-//       const removeContact = await contacts.removeContactsById(id);
-//       console.log(removeContact);
-//       break;
-
-//     case "updateById":
-//       const updateContact = await contacts.updateById(id, {
-//         name,
-//         email,
-//         phone,
-//       });
-//       console.log(updateContact);
-//       break;
-
-//     default:
-//       console.warn("\x1B[31m Unknown action type!");
-
-//       break;
-//   }
-// };
-
-// const arr = hideBin(process.argv);
-// const { argv } = yargs(arr);
-// console.log(argv);
-// invokeAction(argv);
-
-// Виклик команд за допомогою "commander"
-
 const contacts = require("./contacts");
 const { Command } = require("commander");
 const program = new Command();
@@ -99,3 +50,56 @@ program
 program.parse();
 const option = program.opts();
 invokeAction(option);
+
+
+
+
+
+// Парсинга аргументів командного рядка за допомогою "yargs"
+
+// const contacts = require("./contacts");
+// const yargs = require("yargs");
+// const { hideBin } = require("yargs/helpers");
+
+// const invokeAction = async ({ action, id, name, email, phone }) => {
+//   switch (action) {
+//     case "list":
+//       const allContscts = await contacts.listContacts();
+//       console.log(allContscts);
+//       break;
+
+//     case "get":
+//       const oneContact = await contacts.getContactById(id);
+//       console.log(oneContact);
+//       break;
+
+//     case "add":
+//       const newContact = await contacts.addContact({ name, email, phone });
+//       console.log(newContact);
+//       break;
+
+//     case "remove":
+//       const removeContact = await contacts.removeContactsById(id);
+//       console.log(removeContact);
+//       break;
+
+//     case "updateById":
+//       const updateContact = await contacts.updateById(id, {
+//         name,
+//         email,
+//         phone,
+//       });
+//       console.log(updateContact);
+//       break;
+
+//     default:
+//       console.warn("\x1B[31m Unknown action type!");
+
+//       break;
+//   }
+// };
+
+// const arr = hideBin(process.argv);
+// const { argv } = yargs(arr);
+// console.log(argv);
+// invokeAction(argv);
